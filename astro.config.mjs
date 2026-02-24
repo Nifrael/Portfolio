ae1ba477f2658184d2a29addd5168f28c43732df
Julien <julien.dev66@gmail.com>
2026-02-24 22:57:15 +0100

    Adding fonts to the project via experimental Astro API

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    experimental: {
+        fonts: [{
+            provider: fontProviders.google(),
+            name: 'Space Grotesk',
+            cssVariable: '--font-title',
+            weights: ['300', '400', '500', '600', '700']
+        },
+        {
+            provider: fontProviders.google(),
+            name: 'Geist',
+            cssVariable: '--font-body',
+            weights: ['100', '200','300', '400', '500', '600', '700']
+        },
+        {
+            provider: fontProviders.google(),
+            name: 'Geist Mono',
+            cssVariable: '--font-body-mono',
+            weights: ['100', '200','300', '400', '500', '600', '700']
+        }
+    ],
+    }
+});
